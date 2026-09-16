6e1ca0804bf0dd647b870ad4b1fb91fd0566f35b
bryanpaniagua <BryanTX172025@gmail.com>
2026-09-16 16:56:09 +0000

    chore task 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+ 
+fn is_even(n: i32) -> bool {
+	n % 2 == 0
+
+}
+	println!("Enter any number");
+	let mut input = String::new();
+	io::stdin().read_line(&mut input);
+	let number: i32 = input.trim().parse().expect("Please type a number!");
+	if number % 2 == 0 {
+		println!("{number} yes");
+	}
+	else {
+		println!("Not Even");
+		
+	}
+
+
+	
 }
