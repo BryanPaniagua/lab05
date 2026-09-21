9e2bd6662a1430de0707b5c9c4412b226240c3ab
Antonio <antoniobns36@gmail.com>
2026-09-21 16:41:21 +0000

    Add digit_sum

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 use std::io;
 fn main() {
  
-fn is_even(n: i32) -> bool {
-	n % 2 == 0
 
-}
 	println!("Enter any number");
 	let mut input = String::new();
 	io::stdin().read_line(&mut input);
 	let number: i32 = input.trim().parse().expect("Please type a number!");
-	if number % 2 == 0 {
-		println!("{number} yes");
-	}
-	else {
-		println!("Not Even");
-		
+	if is_even(number) { 
+		println!("Even");
 	}
 
+	println!("Enter numbers");
+	let mut input = String::new();
+	io::stdin().read_line(&mut input);
+	let number: i32 = input.trim().parse().expect("Please type a number!");
+	println!("Sum of digits: {}", digit_sum(number));
+}
 
-	
+fn is_even(n: i32) -> bool {
+	n % 2 == 0
+
+}
+
+fn digit_sum(mut number: i32) -> i32 {	
+	let mut sum = 0;
+	while number > 0 {
+		sum += number % 10;
+		number /= 10;
+	}
+        sum
 }
